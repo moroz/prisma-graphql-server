7fb98c57e662af6c17b80896cdfb2807161cbc28
Karol Moroz <k.j.moroz@gmail.com>
2021-12-22 01:04:42 +0100

    Preload user assocs in resolver

--- src/gql/resolvers/index.ts
+++ src/gql/resolvers/index.ts
@@ -4,7 +4,7 @@ import scalars from "./scalars";
 
 const Query: Query = {
   users: async (_parent, _args, { prisma }: Context) => {
-    return prisma.user.findMany();
+    return prisma.user.findMany({ include: { posts: true, profile: true } });
   }
 };
 
